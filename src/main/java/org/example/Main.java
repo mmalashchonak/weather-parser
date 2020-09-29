91feabacc0708c598d90e354cbc3c41fa021f258
mmalashchonak <mmalashchonak@gmail.com>
2020-09-30 02:57:02 +0300

    hw 7 done, Exception catched. mvn corrected. Logger added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,36 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.util.WeatherInfo;
 import org.example.util.WeatherParser;
 
-/**Main class of weather html parser program using Jsoup.
+/**
+ * Main class of weather html parser program using Jsoup.
+ *
  * @author mmalashchonak
  * @version 1.0
  */
 public class Main {
+    /**
+     * Log4j2 logger.
+     */
+    private static final Logger logger = LogManager.getLogger();
 
-  /**
-   * Program entry point.
-   */
-  public static void main(String... args) {
-    WeatherInfo weatherInfo = new WeatherParser();
-    System.out.println(weatherInfo.getWeatherInfo());
-  }
+    /**
+     * String with current date and temperature;
+     */
+    private static String weather;
+
+    /**
+     * Program entry point.
+     */
+    public static void main(String... args) {
+        logger.info("Log4j2 started.");
+        WeatherInfo weatherInfo = new WeatherParser();
+        logger.info("WeatherParser created.");
+        weather = weatherInfo.getWeatherInfo();
+        System.out.println(weather);
+        logger.info("Weather received: " + weather);
+    }
 }
