9ed0f1e5c04e4fb8c0be80c95618b7d736725924
mmalashchonak <mmalashchonak@gmail.com>
2020-10-06 01:55:19 +0300

    Logger log4j2 added to WetherParser.java.

--- src/main/java/org/example/util/WeatherParser.java
+++ src/main/java/org/example/util/WeatherParser.java
@@ -1,5 +1,7 @@
 package org.example.util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -14,20 +16,30 @@ import java.net.URL;
 public class WeatherParser implements WeatherInfo {
 
     /**
+     * Creating log4j2 logger.
+     */
+    private static final Logger logger = LogManager.getLogger();
+
+    /**
      * Build Jsoup Document from html page for further parsing.
      * @return Document
      * @throws RuntimeException If html page can not be downloaded. Caused by MalformedURLException.
      */
     private Document getPage() {
         String url = "https://www.gismeteo.by/weather-minsk-4248/";
+        logger.info("Url string created.");
         Document page = null;
+        logger.info("Null Jsoup document created.");
         try {
+            logger.info("Trying to parse html page.");
             page = Jsoup.parse(new URL(url), 3000);
         } catch (IOException e) {
+            logger.error("Html page parsing failed.");
             System.out.println("404 not found.");
             System.out.println("Сегодня: Вт, 1 янв \nТемпература: -100 +100");
             System.exit(0);
         }
+        logger.info("Html page successfully parsed..");
         return page;
     }
 
@@ -36,12 +48,14 @@ public class WeatherParser implements WeatherInfo {
      * @return String
      */
     public String getWeatherInfo() {
+        logger.info("Building Jsoup Document from html page.");
         Document page = getPage();
+        logger.info("Searching for weather tags in Jsoup Document.");
         Elements todayForecast = page.select("div[class=tabs _center]").select("div[class=tab  tooltip]");
         String date = todayForecast.select("div[class=date]").text();
         Elements todayTemperatures = todayForecast.select("div[class=tabtempline tabtemp_0line clearfix]")
                 .select("span[class=unit unit_temperature_c]");
-
+        logger.info("Today'w weather successfully found. Printing in console.");
         return "Сегодня: " + date + "\n" +
                "Температура: " + todayTemperatures.get(0).text() + " " + todayTemperatures.get(1).text();
     }
