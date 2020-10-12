054f00d94fa71655983ec9171e9b4703de689b6e
mmalashchonak <mmalashchonak@gmail.com>
2020-10-12 23:43:00 +0300

    Program settings added as .properties and .yml files.

--- src/main/java/org/example/db/impl/Database.java
+++ src/main/java/org/example/db/impl/Database.java
@@ -2,10 +2,22 @@ package org.example.db.impl;
 
 public interface Database {
 
+    /**
+     * Put date and temperature as single weather entry into database.
+     *
+     * @param day
+     * @param weather
+     */
     void putWeatherIntoDB(String day, String weather);
 
+    /**
+     * Print last weather from database in console.
+     */
     void printLastWeatherFromDB();
 
+    /**
+     * Close connection to database.
+     */
     void closeConnection();
 
 }
--- src/main/java/org/example/db/impl/MongoDB.java
+++ src/main/java/org/example/db/impl/MongoDB.java
@@ -1,5 +1,8 @@
 package org.example.db.impl;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
@@ -7,25 +10,39 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.io.File;
+import java.io.IOException;
+
 
 public class MongoDB implements Database {
 
-    private static final String Mongo_URI = "mongodb+srv://root:root@cluster0.zhvgt.mongodb.net/test";
+    private String Mongo_URI;
     private static Database mongoDB = new MongoDB();
     private MongoClient client;
     private MongoCollection collection;
+
+    /**
+     * Read MongoDB settings from .properties and initialise fields.
+     */ {
+        try {
+            ObjectMapper om = new ObjectMapper(new YAMLFactory());
+            om.findAndRegisterModules();
+            Mongo_URI = om.readValue(new File("properties.yaml"), Properties.class).Mongo_URI;
+            client = new MongoClient(new MongoClientURI(Mongo_URI));
+            MongoDatabase database = client.getDatabase("test");
+            collection = database.getCollection("test");
+        } catch (IOException ex) {
+            if (client != null) {client.close();}
+            ex.printStackTrace();
+        }
+    }
+
 
     public MongoDB() {
     }
 
     public static Database getDatabase() {
         return mongoDB;
-    }
-
-    {
-        client = new MongoClient(new MongoClientURI(Mongo_URI));
-        MongoDatabase database = client.getDatabase("test");
-        collection = database.getCollection("test");
     }
 
     @Override
@@ -47,4 +64,9 @@ public class MongoDB implements Database {
             client.close();
         }
     }
-}
+
+    static class Properties {
+        @JsonProperty("Mongo_URI")
+        String Mongo_URI;
+        }
+    }
